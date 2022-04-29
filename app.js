3b5236ac18c88b29c8a439602bba5f4871c5e7ef
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2022-04-29 18:24:05 +0000

    Funcs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 (function () {
-	var selectedHash = location.hash.substring(1)
-	if (selectedHash) {
+	function restoreOpenedStateFromUrl() {
+		let selectedHash = location.hash.substring(1)
+		if (selectedHash) {
 		let hashEl = document.getElementById(selectedHash)
 		if (hashEl) {
 			let button = document.querySelector(`.control[data-id="${selectedHash}"]`);	
@@ -12,6 +13,37 @@
 
 		}
 	}
+	}
+
+	function restoreThemeFromStorage() {
+		let theme = localStorage.getItem('theme')
+		if (!theme) { 
+			localStorage.setItem('theme', 'dark'); } 
+		else if (theme === 'light') {
+			document.body.classList.add("light-mode");
+		}
+		
+	}
+
+	function restoreThemeFromURLParams() {
+		let theme = localStorage.getItem('theme')
+		if (!theme) localStorage.setItem('theme', 'dark')
+	}
+	
+	function updateTheme(el=document.body) {
+		var mode = el.classList.contains('light-mode');
+		if (mode) {
+			localStorage.setItem('theme', 'light')
+		} else {
+			localStorage.setItem('theme', 'dark')
+		}
+	}
+
+	// Setup
+	restoreThemeFromStorage();
+	restoreThemeFromURLParams();
+	restoreOpenedStateFromUrl();
+	
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
             document.querySelector(".active-btn").classList.remove("active-btn");
@@ -22,7 +54,9 @@
 			location.hash = button.dataset.id;
         })
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
+
+	document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
+		updateTheme();	
     })
 })();
