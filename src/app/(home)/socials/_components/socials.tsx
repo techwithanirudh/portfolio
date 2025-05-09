dd98099a0d26f0b157ec157e7fadb6fb2f132ee7
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-05-09 11:33:44 +0000

    fix

--- src/app/(home)/socials/_components/socials.tsx
+++ src/app/(home)/socials/_components/socials.tsx
@@ -4,6 +4,7 @@ import type { Social } from '@/app/layout.config';
 import { InlineLink } from '@/components/inline-link';
 import { Section } from '@/components/section';
 import { cva } from 'class-variance-authority';
+import { ExternalLink } from 'lucide-react';
 
 const Socials = ({ socials }: { socials: Social[] }) => (
   <Section className='relative w-full pb-4'>
@@ -11,7 +12,7 @@ const Socials = ({ socials }: { socials: Social[] }) => (
       {socials.map((social) => (
         <a
           key={social.url}
-          className={'group flex flex-col justify-between gap-10 p-6 last:border-border last:border-b last:border-dashed hover:bg-card hover:bg-card/80 sm:gap-22 md:gap-34 lg:gap-46 [&_svg]:size-8 [&_svg]:stroke-1'}
+          className={'group flex flex-col justify-between gap-10 p-6 last:border-border last:border-b last:border-dashed hover:bg-card hover:bg-card/80 sm:gap-22 md:gap-34 lg:gap-46 [&_svg]:size-8 [&_svg]:stroke-1 group relative'}
           href={social.url}
           target='_blank'
           rel='noopener noreferrer'
@@ -25,6 +26,10 @@ const Socials = ({ socials }: { socials: Social[] }) => (
               {social.description}
             </p>
           </div>
+
+          {/* <div className="bg-muted rounded-full p-2 opacity-0 group-hover:opacity-100 transition-opacity absolute right-4 top-4 [&_svg]:size-4! [&_svg]:stroke-1">
+            <ExternalLink />
+          </div> */}
         </a>
       ))}
     </div>
