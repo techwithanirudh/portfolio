86316a7574e9079e452884f22f09eca870db0665
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-05-09 10:25:21 +0000

    f

--- src/app/(home)/socials/page.tsx
+++ src/app/(home)/socials/page.tsx
@@ -2,7 +2,7 @@ import { Section } from '@/components/section';
 import { createMetadata } from '@/lib/metadata';
 import type { Metadata } from 'next';
 import CTA from '../_components/cta';
-import { ContactForm } from './_components/contact-form';
+import { ContactForm } from './components/contact-form';
 
 export default function Contact(): React.ReactElement {
   return (
@@ -10,10 +10,11 @@ export default function Contact(): React.ReactElement {
       <Section className='grid divide-y divide-dashed divide-border lg:grid-cols-2 lg:divide-x lg:divide-y-0'>
         <div className='flex flex-col gap-2 px-6 py-10 md:py-14'>
           <h4 className='max-w-xl text-left font-regular text-3xl tracking-tighter md:text-5xl'>
-            Contact Me
+            Contact
           </h4>
           <p className='max-w-xl text-left text-lg text-muted-foreground leading-relaxed tracking-tight lg:max-w-lg'>
-            Have a question or want to connect? Send a message and expect a response within a week.
+            Let us know if you have any questions or feedback. We are here to
+            help you.
           </p>
         </div>
 
