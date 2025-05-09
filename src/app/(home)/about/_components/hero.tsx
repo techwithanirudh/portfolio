303065561895c058077984156d9b8853124edcff
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-05-09 16:48:31 +0000

    refactor: restructure CTA component for improved layout and readability

--- src/app/(home)/about/_components/hero.tsx
+++ src/app/(home)/about/_components/hero.tsx
@@ -7,16 +7,31 @@ import type React from "react";
 import Balancer from "react-wrap-balancer";
 import * as motion from "motion/react-client";
 import Image from "next/image";
-
 export default function CTA(): React.ReactElement {
   return (
-    <Section className="flex flex-wrap items-center divide-y divide-dashed divide-border lg:divide-x lg:divide-y-0">
-      <div className="flex w-fit items-center p-6  justify-center">
+    <Section
+      className='grid divide-y divide-dashed divide-border lg:grid-cols-2 lg:divide-x lg:divide-y-0'
+    >
+      {/* text side */}
+        <div className='flex flex-col gap-2 px-6 py-10 md:py-14'>
+        <h4 className="text-3xl md:text-5xl font-regular tracking-tighter">
+          The Moderator
+        </h4>
+        <p className="mt-4 text-lg text-muted-foreground leading-relaxed max-w-lg">
+          I’m a community builder who loves transforming small forums into
+          vibrant hangouts. I’m also a Discourse moderator, skilled in managing
+          categories, plugins, and keeping conversations welcoming. When I’m not
+          handling forum tasks, I play chess and binge‑watch YouTube.
+        </p>
+      </div>
+
+      {/* image side */}
+      <div className="p-6 flex items-center justify-center">
         <motion.div
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="relative rounded-md overflow-hidden p-1 border-2 border-border shadow-sm transition-transform hover:scale-110 "
+          className="relative rounded-md overflow-hidden p-1 border-2 border-border shadow-sm hover:scale-110 transition-transform flex items-center justify-center"
         >
           <Image
             src="/icon.png"
@@ -27,18 +42,6 @@ export default function CTA(): React.ReactElement {
             priority
           />
         </motion.div>
-      </div>
-
-      <div className="flex flex-col gap-2 p-6 flex-1 justify-center">
-        <h4 className="max-w-xl text-left font-regular text-3xl tracking-tighter md:text-5xl">
-          The Moderator
-        </h4>
-        <p className="max-w-xl text-left text-lg text-muted-foreground leading-relaxed tracking-tight lg:max-w-lg">
-          I’m a community builder who loves transforming small forums into
-          vibrant hangouts. I’m also a Discourse moderator, skilled in managing
-          categories, plugins, and keeping conversations welcoming. When I’m not
-          handling forum tasks, I play chess and binge-watch YouTube.
-        </p>
       </div>
     </Section>
   );
