0f522b2ecbebe8706f45d6a04fdb4f5dc984802b
Anirudh Sriram <hello@techwithanirudh.com>
2025-05-15 08:04:16 +0000

    Refactor CTA component: update heading and description for clarity, adjust layout for better alignment

--- src/app/(home)/_components/cta.tsx
+++ src/app/(home)/_components/cta.tsx
@@ -9,35 +9,24 @@ import Balancer from 'react-wrap-balancer';
 export default function CTA(): React.ReactElement {
   return (
     <Section className='p-4'>
-      <div className="grid gap-4 rounded-xl border bg-card relative flex flex-col items-center justify-center gap-6  p-8 shadow-sm sm:grid-cols-2 sm:gap-8 sm:p-16">
-        <h2 className="mt-0 mb-4 font-semibold text-3xl tracking-tighter sm:text-5xl">
-          <Balancer>Build your product roadmap at lightspeed</Balancer>
+      <div className='grid gap-4 rounded-xl border bg-card items-center justify-center p-8 shadow-sm sm:p-16'>
+        <h2 className='font-regular text-3xl text-center tracking-tighter sm:text-5xl max-w-xl'>
+          <Balancer> Let's Collaborate</Balancer>
         </h2>
-        <div className="flex flex-col items-start gap-4">
-          <p className="text-muted-foreground sm:text-xl">
+        <div className='flex flex-col items-center gap-4'>
+          <p className='text-muted-foreground sm:text-xl max-w-2xl text-center'>
             <Balancer>
-              Explore problems, ideate solutions, prioritize features and plan
-              your roadmap with the help of AI.
+              Have questions or want to work together? Reach out through the
+              contact form or find me on social platforms.
             </Balancer>
           </p>
-          <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-          <Button size='lg' className='group gap-4' asChild>
-            <Link href={'/contact'}>
-              <Icons.mail className='group-hover:-rotate-12 size-4 transition-transform' />
-              Contact Me{' '}
-            </Link>
-          </Button>
-          <Button
-            size='lg'
-            className='group gap-4 bg-transparent shadow-none'
-            variant='outline'
-            asChild
-          >
-            <Link href='/socials'>
-              Socials{' '}
-              <Icons.arrowRight className='group-hover:-rotate-45 size-4 transition-transform' />
-            </Link>
-          </Button>
+          <div className='flex flex-col gap-4 sm:flex-row sm:items-center'>
+            <Button size='lg' className='group gap-4' asChild>
+              <Link href={'/contact'}>
+                Contact Me{' '}
+                <Icons.arrowRight className='group-hover:-rotate-45 size-4 transition-transform' />
+              </Link>
+            </Button>
           </div>
         </div>
       </div>
