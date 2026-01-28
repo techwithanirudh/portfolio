8cde2a8ed5806a9b064b3ae1e62576acdbd75fc8
Anirudh Sriram <techwithanirudh@gmail.com>
2026-01-28 14:36:37 +0000

    feat: update default siteUrl to localhost in sitemap configuration

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -2,7 +2,7 @@ import type { IConfig } from 'next-sitemap'
 import { env } from './src/env.ts'
 
 const config: IConfig = {
-  siteUrl: env.NEXT_PUBLIC_BASE_URL || 'https://example.com',
+  siteUrl: env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
   generateRobotsTxt: true,
 }
 
