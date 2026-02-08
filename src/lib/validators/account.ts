041b43fff2526f9155c68354ec79fabf4c7f6a1e
Anirudh Sriram <techwithanirudh@gmail.com>
2026-02-08 08:23:47 +0000

    feat: add trim validation to account name schema

--- src/lib/validators/account.ts
+++ src/lib/validators/account.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 export const AccountSchema = z.object({
   name: z
     .string()
+    .trim()
     .min(1, { message: 'Name is required.' })
     .max(32, { message: 'Name must not be longer than 32 characters.' }),
 })
